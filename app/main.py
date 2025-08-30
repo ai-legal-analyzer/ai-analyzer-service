e074d9e98e2f1520c7d101099c423e866ad70981
artem-commits <artem.04k@mail.ru>
2025-08-30 23:27:40 +0700

    Added CORS

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import analyzer
 from app.backend.db import init_db
 import logging
@@ -7,6 +8,23 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+# CORS middleware ДО роутеров!
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 app.include_router(analyzer.router)
 
 
